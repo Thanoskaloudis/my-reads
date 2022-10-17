5d9f46cd0eecb2147101d7d5ea27368f42c10e2d
Thanoskaloudis <8anos.kal@gmail.com>
2022-10-17 21:00:37 +0100

    render books from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import * as BooksAPI from "./utils/BooksAPI";
 import './App.css';
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const getBooks = async () => {
+      const res = await BooksAPI.getAll();
+      setBooks(res);
+    };
+
+    getBooks();
+  }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
+        <h2>My Reads</h2>
       </header>
+      {books.map((book: any) => 
+        <div>
+          <img src={book.imageLinks.thumbnail}/>
+        </div>
+      )}
     </div>
   );
 }
